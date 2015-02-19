3c80a80e6164f4fd40c3fc0ab1b233c98dc6fbf0
harish-sharma <harish.sharma@inmobi.com>
2015-02-19 11:51:12 +0530

    Base dirs

---
+++ src/main/java/com/harish/JavaFun.java
@@ -0,0 +1,11 @@
+package com.harish;
+
+/**
+ * @author harish.sharma
+ */
+public class JavaFun {
+
+    public static void main(String[] arg) {
+        System.out.println("Hello");
+    }
+}
---
+++ src/main/scala/com/harish/ScalaFun.scala
@@ -0,0 +1,10 @@
+package com.harish
+
+/**
+ * @author harish.sharma
+ */
+object ScalaFun {
+  def main(args: Array[String]) {
+    println("Hello")
+  }
+}
