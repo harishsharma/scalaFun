887be7477cab5349d545407409a2bf3a7a239d0f
harish-sharma <harish.sharma@inmobi.com>
2015-02-20 16:35:18 +0530

    Base dirs

--- src/main/java/com/harish/JavaFun.java
+++ src/main/java/com/harish/JavaFun.java
@@ -1,11 +1,68 @@
 package com.harish;
 
+import rx.Observable;
+import rx.Subscriber;
+import rx.functions.Func1;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author harish.sharma
  */
 public class JavaFun {
 
+    public static Observable<Integer> obs(List<Integer> list) {
+
+        return Observable.create(new Observable.OnSubscribe<Integer>() {
+            @Override
+            public void call(Subscriber<? super Integer> subscriber) {
+                try {
+                    for (Integer i : list) {
+                        subscriber.onNext(i);
+                    }
+                    subscriber.onCompleted();
+                } catch (Throwable e) {
+                    subscriber.onError(e);
+                }
+            }
+        });
+    }
+
     public static void main(String[] arg) {
-        System.out.println("Hello");
+        List<Integer> list = new ArrayList() {{
+            add(1);
+            add(2);
+        }};
+        Observable<Integer> listObs = obs(list);
+
+        Subscriber<Integer> subs = new Subscriber<Integer>() {
+            @Override
+            public void onCompleted() {
+                System.out.println("Finished");
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                System.out.println("Exception " + e);
+            }
+
+            @Override
+            public void onNext(Integer integer) {
+                System.out.println("Thread " + Thread.currentThread().getName() + " : int " + integer);
+            }
+        };
+        listObs.subscribe(subs);
+
+        Observable<Integer> map = listObs.map(new Func1<Integer, Integer>() {
+
+            @Override
+            public Integer call(Integer integer) {
+                return integer * 2;
+            }
+        });
+
+        map.subscribe(subs);
+
     }
 }
--- src/main/scala/com/harish/ScalaFun.scala
+++ src/main/scala/com/harish/ScalaFun.scala
@@ -1,10 +1,39 @@
 package com.harish
 
+import rx.lang.scala.{Observable, Subscription}
+
 /**
  * @author harish.sharma
  */
 object ScalaFun {
+
+  def nothing: Observable[Nothing] = Observable[Nothing](observer => {
+    Subscription()
+  })
+
+  def error[T](e: Throwable): Observable[T] = Observable[T](observer => {
+    observer.onError(e)
+    Subscription()
+  })
+
+  def startsWith[T](ss: T*): Observable[T] = {
+    Observable[T](observer => {
+      for (s <- ss) observer.onNext(s)
+      Subscription.apply(observer)
+    })
+  }
+
+  def filter[T](f: T => Boolean): Observable[T] = {
+    null
+  }
+
   def main(args: Array[String]) {
-    println("Hello")
+    val nums = List(1, 2, 3, 4)
+    val obs: Observable[Int] = fun(nums)
+    obs.subscribe(num => println(num))
+  }
+
+  def fun(col: List[Int]): Observable[Int] = {
+    Observable.from(col)
   }
 }
